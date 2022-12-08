b927bacac147f3924ba0a6a70f15a03031124812
Adou YAPO AKO <adou.professionnel@gmail.com>
2022-12-08 14:01:30 +0100

    Create manager

---
+++ main.py
@@ -0,0 +1,16 @@
+menu = {
+    1: "Create a student",
+    2: "Select a student"
+}
+
+
+while True:
+    
+    [print(f"{element[0]} - {element[1]}") for element in menu.items()]
+    
+    choice = input('Press a number to select the right menu')
+    
+    
+    
+    
+
